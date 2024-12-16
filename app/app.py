cb6b53cf5f43743327dae6d851f7a5e795500d80
nhonnguyen <nhontrnguyen@gmail.com>
2024-12-17 05:43:07 +0700

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/add/<int:a>/<int:b>')
+def add(a, b):
+    return jsonify(result=a + b)
+
+if __name__ == '__main__':
+    app.run(debug=True)
