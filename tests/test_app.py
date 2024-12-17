89a06612dba7e4cbc74e06268a5955b3ece2d075
nhonnguyen <nhontrnguyen@gmail.com>
2024-12-17 09:53:50 +0700

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,6 @@ from app.app import app
 
 def test_add():
     client = app.test_client()
-    response = client.get('/add/2/4')
+    response = client.get('/add/3/4')
     assert response.status_code == 200
     assert b'{"result":7}' in response.data
